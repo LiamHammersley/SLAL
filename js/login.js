4afe5d8383c1f782fc505f2fb77919e79e9926ae
Liam Hammersley <liam@liamjh.com>
2014-08-23 17:42:56 +0100

    Styling of Profile page for Mobile.
    Still implementing MQ's to get styling for desktop to work properly.

--- js/login.js
+++ js/login.js
@@ -1,6 +1,3 @@
-//Problem: When clicking on an image user goes to a dead end
-//Soultion: create and overlay with the large image - Lightbox
-
 var $overlay = $('<div id="overlay"></div>');
 var $login = $('<div class="login"><div id="login-close"><span><a href="#" id="login-close">X</a></span></div><div id="login-header"><h1>Login</h1><p>To gain acess to all the awesome location information please login with your username / email address and password.</p></div><div id="login-content"><input type="text" id="login-inputUsername" value="Username or E-Mail"><br><input type="password" id="login-inputPassword" value="Password"><br><input type="checkbox" name="login-checkboxRememberMe"><span>&nbsp;Remember me?</span><br></div><div><button id="login-buttonLogin">Login</button></div><nav id="login-reg-forgot"><a href="#">Register</a><a href="#">Forgotten Password?</a></nav></div>');
 
@@ -19,11 +16,14 @@ $("#js-LoginCapture").click(function (event) {
 
 });
 
+
+//Close Overlay and Login box when clicking X within Login Box
 $("#login-close").click(function () {
   $overlay.hide();
   $login.hide();
 });
 
+//Close Overlay and Login box when clicking on Overlay
 $("#overlay").click(function () {
   $overlay.hide();
   $login.hide();
